b8b4dfac3a55329e868a9d8c76d84b233daec50c
Oskar Ankarberg <oankarberg@gmail.com>
2018-02-13 11:33:07 +0100

    Available for android RN > 0.47 without createJSModule

--- android/src/main/java/com/geektime/rnonesignalandroid/ReactNativeOneSignalPackage.java
+++ android/src/main/java/com/geektime/rnonesignalandroid/ReactNativeOneSignalPackage.java
@@ -26,10 +26,10 @@ public class ReactNativeOneSignalPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
+    // @Override
+    // public List<Class<? extends JavaScriptModule>> createJSModules() {
+    //     return Collections.emptyList();
+    // }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
